6c8e58131f13b676c8681857c2a70c0b9088ea98
madhubn <madhubn.17@gmail.com>
2022-02-10 13:48:42 +0530

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,20 @@ import { HelloComponent } from './hello.component';
 import { AppService } from './app.service';
 import { TreeComponent } from './tree/tree.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularTreeTableModule } from 'angular-tree-table';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable/lib/ngx-datatable.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AngularTreeTableModule,
+    NgxDatatableModule.forRoot({
+      messages: {
+        emptyMessage: 'No data to display', // Message to show when array is presented, but contains no values
+        totalMessage: 'total', // Footer total message
+        selectedMessage: 'selected', // Footer selected message
+      },
+    }),
   ],
   declarations: [AppComponent, HelloComponent, TreeComponent],
   exports: [TreeComponent],
--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,193 +1,61 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import {
-  ExpandableArrowPlacement,
-  ExpandableType,
-  TreeTableData,
-  TreeTableDataConfig,
-  TreeTableHeaderObject,
-  TreeTableRow,
-} from 'angular-tree-table';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.css'],
 })
-export class TreeComponent implements OnInit {
-  tableData: TreeTableData = null;
-  tableConfig: TreeTableDataConfig = {
-    showExpandArrows: true, // To show expand/ collapse button on each row wherever child available
-    showExpandAllArrows: true, // To show Expand/ collapse all button
-    columnVisibility: false, // To show columns visibility options on table
-    columnVisibilityDropDown: true, // To show columns visibility options on table as a popover
-    expandableType: ExpandableType.DIFFERENT_HEADERS,
-    expandableArrowPlacement: ExpandableArrowPlacement.FIRST_COLUMN,
-  };
-  tableHeaders: TreeTableHeaderObject[] = [];
-  months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-    'Kumuliert',
-    'Gesamt',
-  ];
+export class TreeComponent {
+  rows = [];
+  lastIndex = 15;
 
-  constructor(private http: HttpClient) {}
+  ColumnMode: any;
 
-  ngOnInit() {
-    this.loadData();
-  }
-
-  loadData() {
-    let data = [];
-    this.tableData = new TreeTableData(this.tableConfig);
-    this.populateHeaders();
-    this.http
-      .get('http://example.com/message/pdsdata/versand_monitor')
-      .subscribe((resp) => {
-        console.log('resp', resp);
-        for (let level1Key in resp['messageResult']) {
-          let rowData = resp['messageResult'][level1Key];
-          if (rowData['null'] === undefined) {
-            if (Object.keys(rowData).length > 0) {
-              rowData['null'] = rowData[Object.keys(rowData)[0]];
-            }
-          }
-          let row = new TreeTableRow(
-            level1Key,
-            {
-              name: level1Key,
-              data: this.addMissedMonths(rowData['null']['null']['null']),
-            },
-            true,
-            undefined
-          );
-          row.defaultExpand = true;
-
-          const subConfig = JSON.parse(JSON.stringify(this.tableConfig));
-          subConfig.expandableType = ExpandableType.SAME_HEADERS;
-          const subTableData = new TreeTableData(subConfig);
-          const subData = [];
-          for (let level2Key in rowData) {
-            if (level2Key === 'null') {
-              continue;
-            }
-            let subRowData = rowData[level2Key];
-            let subRow = new TreeTableRow(
-              level2Key,
-              {
-                name: level2Key,
-                data: this.addMissedMonths(subRowData['null']['null']),
-              },
-              true,
-              undefined
-            );
-
-            const subSubConfig = JSON.parse(JSON.stringify(subConfig));
-            subSubConfig.expandableType = ExpandableType.SAME_HEADERS;
-            const subSubTableData = new TreeTableData(subSubConfig);
-            const subSubData = [];
-            for (let level3Key in subRowData) {
-              if (level3Key === 'null') {
-                continue;
-              }
-              let subSubRowData = subRowData[level3Key];
-              let subSubRow = new TreeTableRow(
-                level3Key,
-                {
-                  name: level3Key,
-                  data: this.addMissedMonths(subSubRowData['null']),
-                },
-                true,
-                undefined
-              );
-
-              const subSubSubConfig = JSON.parse(JSON.stringify(subSubConfig));
-              subSubSubConfig.expandableType = ExpandableType.SAME_HEADERS;
-              const subSubSubTableData = new TreeTableData(subSubSubConfig);
-              const subSubSubData = [];
-              for (let accountName in subSubRowData) {
-                if (accountName === 'null') {
-                  continue;
-                }
-                let subSubSubRowData = subSubRowData[accountName];
-                let subSubSubRow = new TreeTableRow(
-                  accountName,
-                  {
-                    name: accountName,
-                    data: this.addMissedMonths(subSubSubRowData),
-                  },
-                  false,
-                  undefined
-                );
-                subSubSubData.push(subSubSubRow);
-              }
-
-              subSubSubTableData.headers = this.tableHeaders;
-              subSubSubTableData.data = subSubSubData;
-              subSubRow.children = subSubSubTableData;
-              subSubData.push(subSubRow);
-            }
-
-            subSubTableData.headers = this.tableHeaders;
-            subSubTableData.data = subSubData;
-            if (subSubTableData.data.length > 0) {
-              subRow.defaultExpand = true;
-            }
-            subRow.children = subSubTableData;
-            subData.push(subRow);
-          }
-          subTableData.headers = this.tableHeaders;
-          subTableData.data = subData;
-          row.children = subTableData;
-
-          data.push(row);
-          data = data.sort((v1: TreeTableRow, v2: TreeTableRow) => {
-            try {
-              if (v1.id.split('-').length > 1 && v2.id.split('-').length > 1) {
-                let y1 = v1.id.split('-')[1];
-                let y2 = v2.id.split('-')[1];
-                return parseInt(y2) - parseInt(y1);
-              }
-              return 0;
-            } catch (e) {
-              return 0;
-            }
-          });
-        }
-        this.tableData.data = data;
+  constructor(private cd: ChangeDetectorRef) {
+    this.fetch((data) => {
+      data = data.slice(1, this.lastIndex);
+      this.rows = data.map((d) => {
+        d.treeStatus = 'collapsed';
+        d.parentId = null;
+        return d;
       });
+    });
   }
 
-  addMissedMonths(data: any) {
-    for (let month of this.months) {
-      if (data[month] === undefined) {
-        data[month] = 0;
-      }
-    }
-    return data;
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/100k.json`);
+
+    req.onload = () => {
+      setTimeout(() => {
+        // cb(JSON.parse(req.response));
+      }, 500);
+    };
+
+    req.send();
   }
 
-  populateHeaders() {
-    this.tableHeaders.splice(0, this.tableHeaders.length);
-    this.tableHeaders.push(
-      new TreeTableHeaderObject('Name', 'name', null, true)
-    );
-    for (let month of this.months) {
-      this.tableHeaders.push(
-        new TreeTableHeaderObject(month, 'data.' + month, null, true)
-      );
+  onTreeAction(event: any) {
+    const index = event.rowIndex;
+    const row = event.row;
+    if (row.treeStatus === 'collapsed') {
+      row.treeStatus = 'loading';
+      this.fetch((data) => {
+        data = data.slice(this.lastIndex, this.lastIndex + 3).map((d) => {
+          d.treeStatus = 'collapsed';
+          d.parentId = row.id;
+          return d;
+        });
+        this.lastIndex = this.lastIndex + 3;
+        row.treeStatus = 'expanded';
+        this.rows = [...this.rows, ...data];
+        this.cd.detectChanges();
+      });
+    } else {
+      row.treeStatus = 'collapsed';
+      this.rows = [...this.rows];
+      this.cd.detectChanges();
     }
-    this.tableData.headers = this.tableHeaders;
   }
 }
